3e0b303ad1e85569fa072536fc65b65c32bef4f3
giladh <gdpstd@gmail.com>
2021-01-05 22:29:03 +0200

    Add show_part_Q1 function

---
+++ matlab/show_part2_Q1.m
@@ -0,0 +1,36 @@
+function [outputArg1,outputArg2] = show_part2_Q1(t,y,I)
+m = y(:,1);
+n = y(:,2);
+h = y(:,3);
+V = y(:,4);
+
+figure();
+subplot(2,2,1)
+plot(t, m)
+title("m(t)")
+xlabel("t[msec]")
+ylabel("m")
+
+subplot(2,2,2)
+plot(t, n)
+title("n(t)")
+xlabel("t[msec]")
+ylabel("n")
+
+subplot(2,2,3)
+plot(t, h)
+title("h(t)")
+xlabel("t[msec]")
+ylabel("h")
+
+subplot(2,2,4)
+plot(t, V)
+title("V(t)")
+xlabel("t[msec]")
+ylabel("V[mV]")
+
+sgtitle_str = "$m, n, h$ and $V$ over time for an input of: $I = " + ...
+              num2str(I) + " \cdot u(t)$";
+sgtitle(sgtitle_str, "interpreter", "latex");
+end
+
